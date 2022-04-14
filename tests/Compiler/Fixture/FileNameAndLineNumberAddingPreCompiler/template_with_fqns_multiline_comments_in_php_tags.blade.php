8cf63b0b7ece870cf5ac846bcb14600ed4b0ed01
Owen Voke <development@voke.dev>
2022-04-14 11:56:08 +0100

    tests: add fixture for FQNS class in docblock

---
+++ tests/Compiler/Fixture/FileNameAndLineNumberAddingPreCompiler/template_with_fqns_multiline_comments_in_php_tags.blade.php
@@ -0,0 +1,13 @@
+@php
+/**
+ * @var \Throwable $throwable
+ * @var bool       $bool
+ */
+@endphp
+-----
+/** file: foo.blade.php, line: 1 */@php
+/**
+ * @var \Throwable $throwable
+ * @var bool       $bool
+ */
+/** file: foo.blade.php, line: 6 */@endphp
